4e925858244a742b4c512888c52c7e2d752090fe
SShipon <nxshipon@gmail.com>
2024-08-17 16:49:15 +0100

    create a main pages desing add

--- src/components/Home/Services/Services.jsx
+++ src/components/Home/Services/Services.jsx
@@ -11,14 +11,14 @@ export default function Services() {
   console.log(data)
   
   return (
-    <div className="container mx-auto  my-10 ">
-       <div className="my-9">
-         <h1 className="text-2xl font-bold text-center mb-4 py-4">OUR SERVICES</h1>
-       </div>
-       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-         
-       </div>
-    
-    </div>
+ 
+     <section>
+       <div className="flex ...">
+       <div  className="w-2/5 ...">w-2/5</div>
+       <div  className="w-3/5 ...">w-3/5</div>
+  
+  </div>
+     </section>
+  
   );
 }
